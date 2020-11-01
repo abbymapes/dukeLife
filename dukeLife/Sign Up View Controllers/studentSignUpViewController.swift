b886d87bbc269dc50e6962e3dcfc3c15187b82b6
Moses <mm687@duke.edu>
2020-11-01 01:23:11 -0400

    fixed Student Signup ready to connect to firebase

--- dukeLife/Sign Up View Controllers/studentSignUpViewController.swift
+++ dukeLife/Sign Up View Controllers/studentSignUpViewController.swift
@@ -6,7 +6,8 @@
 //
 
 import UIKit
-
+import Firebase
+import FirebaseAuth
 class studentSignUpViewController: UIViewController {
 
     override func viewDidLoad() {
@@ -15,18 +16,39 @@ class studentSignUpViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
 
-    @IBAction func Duke_email(_ sender: UITextField) {
-    }
+
     
-    @IBAction func netID(_ sender: UITextField) {
-    }
+    @IBOutlet weak var Duke_Email: UITextField!
+    @IBOutlet weak var netID: UITextField!
+
+    @IBOutlet weak var Password_stud: UITextField!
+    @IBOutlet weak var Confirm_Password_Stud: UITextField!
     
-    @IBAction func PassWord(_ sender: UITextField) {
+    
+    @IBAction func SignUp_Button_stud(_ sender: Any) {
+        
+        if Duke_Email.text?.isEmpty == true{
+            print(" Please Insert a valid Duke email ")
+            return
+        }
+        
+        if netID.text?.isEmpty == true{
+            print(" Please Insert a valid netID ")
+            return
+        }
+        if Password_stud.text?.isEmpty == true{
+            print(" Please Insert a password ")
+            return
+        }
+        
+        if Confirm_Password_Stud.text?.isEmpty == true {
+            print("Please confirm password")
+            return
     }
-    @IBAction func Confirm_Password(_ sender: UITextField) {
-    }
-    @IBAction func SignUp_Button(_ sender: Any) {
-    }
+        if Password_stud.text != Confirm_Password_Stud.text == true {
+            print("Make sure passwords match")
+            return
+        }
     
     /*
     // MARK: - Navigation
