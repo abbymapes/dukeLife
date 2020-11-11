6c53c076c5c8098828cd24c12e1e8c88bf84d4a1
icg2 <icg2@duke.edu>
2020-11-11 12:19:07 -0500

    Finished extra images

--- dukeLife/Guest View Controllers/guestImageCollectionViewController.swift
+++ dukeLife/Guest View Controllers/guestImageCollectionViewController.swift
@@ -91,25 +91,11 @@ class guestImageCollectionViewController: UICollectionViewController {
     
 
     // MARK: UICollectionViewDelegate
+    override func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+            self.performSegue(withIdentifier: "singleImageSegue", sender: indexPath)
 
-    
-    // Uncomment this method to specify if the specified item should be highlighted during tracking
-    override func collectionView(_ collectionView: UICollectionView, shouldHighlightItemAt indexPath: IndexPath) -> Bool {
-        return true
     }
     
-    /*
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-
-         if let cell = sender as? UICollectionViewCell,
-            let indexPath = self.collectionView.indexPath(for: cell) {
-
-             let vc = segue.destination as! ScrollViewController //Cast with your DestinationController
-             //Now simply set the title property of vc
-             vc.img = images[indexPath.row]
-         }
-    }
- */
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         if segue.identifier == "singleImageSegue"{
             let selectedIndexPath = sender as? NSIndexPath
@@ -122,150 +108,7 @@ class guestImageCollectionViewController: UICollectionViewController {
 
     
     // Uncomment this method to specify if the specified item should be selected
-
-    
     override func collectionView(_ collectionView: UICollectionView, shouldSelectItemAt indexPath: IndexPath) -> Bool {
         return true
     }
-    
-    /*
-    // Uncomment these methods to specify if an action menu should be displayed for the specified item, and react to actions performed on the item
-    override func collectionView(_ collectionView: UICollectionView, shouldShowMenuForItemAt indexPath: IndexPath) -> Bool {
-        return false
-    }
-
-    override func collectionView(_ collectionView: UICollectionView, canPerformAction action: Selector, forItemAt indexPath: IndexPath, withSender sender: Any?) -> Bool {
-        return false
-    }
-
-    override func collectionView(_ collectionView: UICollectionView, performAction action: Selector, forItemAt indexPath: IndexPath, withSender sender: Any?) {
-    
-    }
-    */
-    override func collectionView(_ collectionView: UICollectionView, viewForSupplementaryElementOfKind kind: String, at indexPath: IndexPath) -> UICollectionReusableView {
-        if (kind == UICollectionView.elementKindSectionFooter) {
-            let footerView = collectionView.dequeueReusableSupplementaryView(ofKind: kind, withReuseIdentifier: "FooterCollectionReusableView", for: indexPath)
-            // Customize footerView here
-            return footerView
-        } else if (kind == UICollectionView.elementKindSectionHeader) {
-            let headerView = collectionView.dequeueReusableSupplementaryView(ofKind: kind, withReuseIdentifier: "HeaderCollectionReusableView", for: indexPath)
-            // Customize headerView here
-            return headerView
-        }
-        fatalError()
-    }
-
-}
-
-//MARK:- Image Picker
-extension guestImageCollectionViewController: UIImagePickerControllerDelegate, UINavigationControllerDelegate {
-
-    //Show alert to selected the media source type.
-    private func showAlert() {
-
-        let alert = UIAlertController(title: "Image Selection", message: "From where you want to pick this image?", preferredStyle: .actionSheet)
-        alert.addAction(UIAlertAction(title: "Camera", style: .default, handler: {(action: UIAlertAction) in
-            self.getImage(fromSourceType: .camera)
-        }))
-        alert.addAction(UIAlertAction(title: "Photo Album", style: .default, handler: {(action: UIAlertAction) in
-            self.getImage(fromSourceType: .photoLibrary)
-        }))
-        alert.addAction(UIAlertAction(title: "Cancel", style: .destructive, handler: nil))
-        self.present(alert, animated: true, completion: nil)
-    }
-
-    //get image from source type
-    private func getImage(fromSourceType sourceType: UIImagePickerController.SourceType) {
-
-        //Check is source type available
-        if UIImagePickerController.isSourceTypeAvailable(sourceType) {
-
-            let imagePickerController = UIImagePickerController()
-            imagePickerController.delegate = self
-            imagePickerController.sourceType = sourceType
-            self.present(imagePickerController, animated: true, completion: nil)
-        }
-    }
-
-    
-    //MARK:- UIImagePickerViewDelegate.
-    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
-
-        self.dismiss(animated: true) { [weak self] in
-
-            guard let image = info[UIImagePickerController.InfoKey.originalImage] as? UIImage else { return }
-            if let imgUrl = info[UIImagePickerController.InfoKey.imageURL] as? URL{
-                    let imgName = imgUrl.lastPathComponent
-                    let documentDirectory = NSSearchPathForDirectoriesInDomains(.documentDirectory, .userDomainMask, true).first
-                    let localPath = documentDirectory?.appending(imgName)
-
-                    let image = info[UIImagePickerController.InfoKey.originalImage] as! UIImage
-                    let data = image.pngData()! as NSData
-                    data.write(toFile: localPath!, atomically: true)
-                    
-                    let storage = Storage.storage()
-                    let storageRef = storage.reference()
-                    // Create a reference to the file you want to upload
-                    let timestamp = NSDate().timeIntervalSince1970
-                    let myTimeInterval = TimeInterval(timestamp)
-                    let time = NSDate(timeIntervalSince1970: TimeInterval(myTimeInterval))
-                    let formatter = DateFormatter()
-                    formatter.dateFormat = "ddMMMyyyyHH:mm"
-                    let uniqueName = self!.currentUserId +  formatter.string(from: time as Date)
-                    let photoRef = storageRef.child("images/\(uniqueName).jpg")
-                
-                self?.images.append(image)
-                DispatchQueue.main.async {[weak self] in
-                    self?.collectionView.reloadData()
-                }
-
-                // Upload the file to storage and store in database
-                let uploadTask = photoRef.putData(data as Data, metadata: nil) { (metadata, error) in
-                      guard let metadata = metadata else {
-                        print("Error uploading \(error)")
-                        return
-                      }
-                      // You can also access to download URL after upload.
-                      photoRef.downloadURL { (url, error) in
-                        guard let downloadURL = url else {
-                            print("Error uploading \(error)")
-                          return
-                        }
-                        let db = Firestore.firestore()
-                            
-                        var ref: DocumentReference? = nil
-                        ref = db.collection("images").addDocument(data: [
-                            "imageUrl": downloadURL.absoluteString,
-                            "placeId": self?.placeId,
-                            "userId": self?.currentUserId
-                        ])
-                        { err in
-                            if let err = err {
-                                print("Error adding document: \(err)")
-                            } else {
-                                print("Image written with Document ID: \(ref!.documentID)")
-                            }
-                        }
-                      }
-                    }
-            }
-        }
-    }
-
-    func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
-        picker.dismiss(animated: true, completion: nil)
-    }
-    
-    /*
-    override func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
-        let vc = storyboard?.instantiateViewController(withIdentifier: "ScrollViewController") as? ScrollViewController
-        vc?.img = images[indexPath.row]
-        self.navigationController?.pushViewController(vc!, animated: true)
-        
-    }
-    */
-    
-    
-
-
 }
