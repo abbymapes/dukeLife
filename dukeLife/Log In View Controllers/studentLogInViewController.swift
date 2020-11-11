4eda64e414809e2e30be9bb6974af1bce6180406
Moses <mm687@duke.edu>
2020-11-11 13:51:41 -0500

    Login not yet working. seems to also be a memory error of sorts with code name : Thread 40: EXC_BAD_ACCESS (code=EXC_I386_GPFLT)

--- dukeLife/Log In View Controllers/studentLogInViewController.swift
+++ dukeLife/Log In View Controllers/studentLogInViewController.swift
@@ -10,6 +10,13 @@ import Firebase
 import FirebaseAuth
 
 class studentLogInViewController: UIViewController {
+    func showAlert(message:String)
+    {
+    let alert = UIAlertController(title: message, message: "", preferredStyle: .alert)
+    alert.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
+    self.present(alert, animated: true)
+    }
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -22,24 +29,48 @@ class studentLogInViewController: UIViewController {
     @IBOutlet weak var password: UITextField!
     
     @IBAction func NetID_field(_ sender: UITextField) {
+        
+        
     }
     
     @IBAction func Password_field(_ sender: UITextField) {
     }
     
     @IBAction func Log_in_button(_ sender: Any) {
-        if email.text?.isEmpty == true {
+  
+        
+        
+        
+        
+    if email.text?.isEmpty == true || password.text?.isEmpty == true
+        {
             print(" Please Insert a valid Duke Email")
+            showAlert(message: " Please Insert email and password")
             return
         }
-        if password.text?.isEmpty == true{
-            print(" Please Insert a password ")
-            return
+        else{
+            Auth.auth().signIn(withEmail: self.email.text!, password: self.password.text!) { (user, error) in
+                        
+                        if error == nil {
+                            
+                            let vc = self.storyboard?.instantiateViewController(withIdentifier: "Home")
+                                            self.present(vc!, animated: true, completion: nil)
+                        }
+                        else {
+                            
+                            //Tells the user that there is an error and then gets firebase to tell them the error
+                                          let alertController = UIAlertController(title: "Error", message: error?.localizedDescription, preferredStyle: .alert)
+                                          
+                                          let defaultAction = UIAlertAction(title: "OK", style: .cancel, handler: nil)
+                                          alertController.addAction(defaultAction)
+                                          
+                                          self.present(alertController, animated: true, completion: nil)
+                          
+                        }
         }
-        Auth.auth().signIn(withEmail: email.text!, password: password.text!) { [weak self] authResult, error in
-          guard let strongSelf = self else { return }
-            
-        }
+        
+        //login()
+        
     }
     
     /*// MARK: - Navigation
@@ -50,5 +81,46 @@ class studentLogInViewController: UIViewController {
         // Pass the selected object to the new view controller.
     }
     */
-
+        /*
+    func login (){
+        Auth.auth().signIn(withEmail: email.text!, password: password.text!) { (user, error) in
+            //if let strongSelf = self { self.showAlert(message: " Please enter correct info ")
+               // return }
+           // if let error = error, user == nil {
+               // print(error.localizedDescription)
+               // self.showAlert(message: " Please Insert a password ")
+            if error == nil {
+                //Print into the console if successfully logged in
+                              print("You have successfully logged in")
+                              
+                              //Go to the HomeViewController if the login is sucessful
+                              let VC = self.storyboard?.instantiateViewController(withIdentifier: "logged")
+                              self.present(VC!, animated: true, completion: nil)
+            }
+            else{
+                //Tells the user that there is an error and then gets firebase to tell them the error
+                              let alertController = UIAlertController(title: "Error", message: error?.localizedDescription, preferredStyle: .alert)
+                              
+                              let defaultAction = UIAlertAction(title: "OK", style: .cancel, handler: nil)
+                              alertController.addAction(defaultAction)
+                              
+                              self.present(alertController, animated: true, completion: nil)
+            }
+            //self?.checkinfo()
+        }
+       
+}
+   */ /*
+    func checkinfo(){
+        if Auth.updateCurrentUser != nil {
+            print(Auth.auth().currentUser?.uid)
+            let storyboard = UIStoryboard(name: "Main", bundle: nil)
+            let vc = storyboard.instantiateViewController(identifier: "logged")
+            vc.modalPresentationStyle = .overFullScreen
+            present(vc,animated: true)
+        }
+    } */
+    
+  
+}
 }
